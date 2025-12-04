1941dc9ab8f50d05bd70dd07b5c2c40dad5ca4ec
kirath <kirathrg123@gmail.com>
2025-12-04 13:48:45 +0530

    345

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,11 @@
 import ProductDetail from "@/views/ProductDetail";
+import { products } from "@/data/products";
+
+export async function generateStaticParams() {
+  return products.map((product) => ({
+    id: product.id,
+  }));
+}
 
 export default function ProductPage() {
   return <ProductDetail />;
